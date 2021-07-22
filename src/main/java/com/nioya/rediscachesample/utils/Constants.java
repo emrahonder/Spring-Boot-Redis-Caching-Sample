fbca87625b2127a530605a66a99ac59dcae71213
Emrah Onder <emrah.onder@getir.com>
2021-07-22 19:16:55 +0300

    Initial Commit

---
+++ src/main/java/com/nioya/rediscachesample/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.nioya.rediscachesample.utils;
+
+public interface Constants {
+
+    interface CacheNames {
+        String ALL_BOOKS = "booksCache";
+        String BOOK = "bookCache";
+    }
+
+}
+
+
